b31dcf04899bd7340487257de632752a538bf233
Stephens Xu <stephensxub@gmail.com>
2014-08-13 21:42:09 -0700

    created user table in database

--- db/migrate/20140814043751_create_users.rb
+++ db/migrate/20140814043751_create_users.rb
@@ -1,10 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :password_digest
+      t.string :email, :null => false
+      t.string :password_digest, :null => false
+      t.string :nick_name
 
-      t.timestamps
+      t.timestamps :null => false
     end
     add_index :users, :email, unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140814022922) do
+ActiveRecord::Schema.define(version: 20140814043751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,5 +28,15 @@ ActiveRecord::Schema.define(version: 20140814022922) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.string   "nick_name"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
