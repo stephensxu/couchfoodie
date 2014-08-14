2743a40198b98aaca712820beabbcedbc5c98873
Stephens Xu <stephensxub@gmail.com>
2014-08-13 22:39:08 -0700

    added users and kitchens resources to routs.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   validates :email, :presence => true, :uniqueness => true, :length => { :minimum => 6 }, :email => true
   validates :password, :presence => true, :length => { :minimum => 6 }, :confirmation => true
-  validates :nick_name, :presence => true, :length => { :minimum => 6 }, 
+  validates :nick_name, :presence => true, :length => { :minimum => 6 },
             :format => { :with => /[\w\s]+/, message: "nickname can only be letters and numbers"}
 
   has_one :kitchen
