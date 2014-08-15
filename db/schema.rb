4026ea4cfa186495b72c526ea4395b811c879696
Stephens Xu <stephensxub@gmail.com>
2014-08-14 23:16:27 -0700

    ran migration create_reservations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140814050650) do
+ActiveRecord::Schema.define(version: 20140815055548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20140814050650) do
   end
 
   add_index "kitchens", ["user_id"], name: "index_kitchens_on_user_id", using: :btree
+
+  create_table "reservations", force: true do |t|
+    t.string   "status",       default: "pending", null: false
+    t.date     "reserve_date",                     null: false
+    t.time     "reserve_time",                     null: false
+    t.string   "message"
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",           null: false
