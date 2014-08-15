41a8561aa9485e98cc18fdd8c248a97805eaceac
Stephens Xu <stephensxub@gmail.com>
2014-08-14 23:31:37 -0700

    added user_id reference to Reservations table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140815055548) do
+ActiveRecord::Schema.define(version: 20140815062942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,10 @@ ActiveRecord::Schema.define(version: 20140815055548) do
     t.string   "message"
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
+    t.integer  "user_id"
   end
+
+  add_index "reservations", ["user_id"], name: "index_reservations_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",           null: false
