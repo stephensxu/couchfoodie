7464d8b98e3d99927df138fba94794b962a663e2
Stephens Xu <stephensxub@gmail.com>
2014-08-14 17:44:46 -0700

    added user controller; added login function to application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,32 @@
 class UsersController < ApplicationController
+
   def index
+    current_user
+    @link = Link.new
+    @links = Link.order('created_at DESC')
     render :index
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
