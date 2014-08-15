56ef96b3df7ebc6cbb7a2ec4e7ab3e20a9e13c44
Stephens Xu <stephensxub@gmail.com>
2014-08-14 23:15:23 -0700

    created migration create_reservations

--- db/migrate/20140815055548_create_reservations.rb
+++ db/migrate/20140815055548_create_reservations.rb
@@ -1,8 +1,12 @@
 class CreateReservations < ActiveRecord::Migration
   def change
     create_table :reservations do |t|
+      t.string :status, :default => "pending", :null => false
+      t.date   :reserve_date, :null => false
+      t.time   :reserve_time, :null => false 
+      t.string :message
 
-      t.timestamps
+      t.timestamps :null => false
     end
   end
 end
