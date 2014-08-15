9931e167c4320b667877ad1b21185e0791acd33e
Stephens Xu <stephensxub@gmail.com>
2014-08-14 23:33:19 -0700

    added kitche_id to Reservations table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140815062942) do
+ActiveRecord::Schema.define(version: 20140815063227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,8 +40,10 @@ ActiveRecord::Schema.define(version: 20140815062942) do
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
     t.integer  "user_id"
+    t.integer  "kitchen_id"
   end
 
+  add_index "reservations", ["kitchen_id"], name: "index_reservations_on_kitchen_id", using: :btree
   add_index "reservations", ["user_id"], name: "index_reservations_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
