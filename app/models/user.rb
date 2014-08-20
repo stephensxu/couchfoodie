58794728ee99b0af86169b9cbc04aff3fe771f7a
Stephens Xu <stephensxub@gmail.com>
2014-08-19 21:49:06 -0700

    added test for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,9 +20,10 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, :presence => true, :uniqueness => true, :length => { :minimum => 6 }, :email => true
+  validates :created_at, :presence => true
   validates :password, :presence => true, :length => { :minimum => 6 }, :confirmation => true
   validates :nickname, :presence => true, :length => { :minimum => 6 }, :uniqueness => true,
-            :format => { :with => /[\w\s]+/, message: "nickname can only be letters and numbers"}
+            :format => { :with => /\A[\w\s]+\z/, message: "nickname can only be letters and numbers"}
 
   has_many :kitchens
   has_many :reservations
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,5 +19,23 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
+  it { should have_many(:kitchens) }
+  it { should have_many(:reservations) }
+  
+  describe "#valid?" do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:nickname) }
+    it { should validate_presence_of(:created_at) }
+    it { should ensure_length_of(:nickname).is_at_least(6) }
+    it { should allow_value("I am King", "abcde123", "915823fb").for(:nickname) }
+    it { should_not allow_value(1, true, false, "#@**#", "195&%$fjg", "@$$%^").for(:nickname) }
+    it { should ensure_length_of(:email).is_at_least(6) }
+    it { should ensure_length_of(:password).is_at_least(6) }
+    it { should allow_value("waffle@gmail.com", "foo+bar@gmail.io").for(:email) }
+    it { should_not allow_value("@", "jim@", "@iams", "stephens@1234ji").for(:email) }
+    it { should allow_value("123456", "abcdefg123", "abcdefgibme", "isigjsdflsj").for(:password) }
+    it { should_not allow_value("1", "123", "12345", "abcde").for(:password) }
+    it { should validate_confirmation_of(:password) }
+  end
 end
-
