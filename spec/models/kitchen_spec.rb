2c8d870d9e6a0ba60e50d007a88735dcbf3b4d91
Stephens Xu <stephensxub@gmail.com>
2014-08-20 18:05:08 -0700

    refactored kitchen test

--- spec/models/kitchen_spec.rb
+++ spec/models/kitchen_spec.rb
@@ -31,25 +31,21 @@ RSpec.describe Kitchen, :type => :model do
 
   describe "#valid?" do
     it { should validate_presence_of(:name) }
-    it { should ensure_length_of(:name).is_at_least(6) }
-    it { should ensure_length_of(:name).is_at_most(50)}
+    it { should ensure_length_of(:name).is_at_least(6).is_at_most(50) }
     it { should allow_value("myktichen123", "someone's kitchen").for(:name) }
     it { should_not allow_value("@123").for(:name) }
     it { should_not allow_value("1234t").for(:name) }
 
     it { should validate_presence_of(:description) }
-    it { should ensure_length_of(:description).is_at_least(10) }
-    it { should ensure_length_of(:description).is_at_most(250) }
+    it { should ensure_length_of(:description).is_at_least(10).is_at_most(250) }
 
     it { should validate_presence_of(:user) }
 
     it { should validate_presence_of(:street_address) }
-    it { should ensure_length_of(:street_address).is_at_least(6) }
-    it { should ensure_length_of(:street_address).is_at_most(50) }
+    it { should ensure_length_of(:street_address).is_at_least(6).is_at_most(50) }
 
     it { should validate_presence_of(:city) }
-    it { should ensure_length_of(:city ).is_at_least(3) }
-    it { should ensure_length_of(:city ).is_at_most(20) }
+    it { should ensure_length_of(:city).is_at_least(3).is_at_most(20) }
 
     it { should validate_presence_of(:state) }
     it { should ensure_length_of(:state).is_equal_to(2) }
@@ -60,9 +56,10 @@ RSpec.describe Kitchen, :type => :model do
     it { should validate_presence_of(:zipcode) }
     it { should ensure_length_of(:zipcode).is_at_least(5) }
     it { should allow_value("95134", "12345-0934", "83592").for(:zipcode) }
-    it { should_not allow_value("1234").for(:zipcode) }
-    it { should_not allow_value("123456").for(:zipcode) }
-    it { should_not allow_value("12345-12345").for(:zipcode) }
+
+    %w(1234 123456 12345-12345).each do |bad_zip|
+      it { should_not allow_value(bad_zip).for(:zipcode) }
+    end
 
     it { should validate_presence_of(:data_status) }
     it { should allow_value("active", "archive").for(:data_status) }
