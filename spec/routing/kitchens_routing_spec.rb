32dedd7450e02b864a85b264cea390b5641d6848
Stephens Xu <stephensxub@gmail.com>
2014-08-21 00:17:51 -0700

    added routing test for kitchens_controller

---
+++ spec/routing/kitchens_routing_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe KitchensController, :type => :routing do
+  describe "routing" do
+
+    it "routes to #index" do
+      expect(:get => "/").to route_to("kitchens#index")
+    end
+
+    it "routes to #new" do
+      expect(:get => "/kitchens/new").to route_to("kitchens#new")
+    end
+
+    it "routes to #show" do
+      expect(:get => "/kitchens/1").to route_to("kitchens#show", :id => "1")
+    end
+
+    it "routes to #create" do
+      expect(:post => "/kitchens").to route_to("kitchens#create")
+    end
+  end
+end
