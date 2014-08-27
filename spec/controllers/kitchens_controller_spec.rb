0475988edac8ca890c5a215bb17e4af9546341d6
Stephens Xu <stephensxub@gmail.com>
2014-08-26 19:27:13 -0700

    wrote test for Kitchen_controller#delete; passing

--- spec/controllers/kitchens_controller_spec.rb
+++ spec/controllers/kitchens_controller_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe KitchensController, :type => :controller do
 
+  let(:valid_attributes) do
+    FactoryGirl.attributes_for(:kitchen)
+  end
+
+  describe "DELETE destroy" do
+    let!(:kitchen) { FactoryGirl.create(:kitchen_with_user) }
+
+    context "the user created the requested kitchen" do
+      it "change the data_status of kitchen to archive" do
+        expect {
+          delete :destroy, { :id => kitchen.id }, { :user_id => kitchen.user.id }
+        }.to change{ kitchen.reload.data_status }.to("archive")
+      end
+
+      it "does not remove the kitchen record from database" do
+        expect {
+          delete :destroy, { :id => kitchen.id }, { :user_id => kitchen.user.id }
+        }.to_not change(Kitchen, :count)
+      end
+    end
+  end
 end
