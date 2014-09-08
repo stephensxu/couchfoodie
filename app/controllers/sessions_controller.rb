caa54e74d3183ac5a6e3529d941ee0964f007ee9
Stephens Xu <stephensxub@gmail.com>
2014-09-07 22:49:50 -0700

    modified sessoin creation method to use facebook authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,27 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_by_email(session_params[:email])
-    if @user && @user.authenticate(session_params[:password])
+    p request.env['omniauth.auth']
+
+    auth = request.env['omniauth.auth']
+    @user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+
+    if @user
       login!(@user)
       redirect_to users_path
     else
-      redirect_to(root_url, notice: "Invalid email or password.")
+      redirect_to(root_url, :notice => "Invalid user credentials")
     end
   end
+
+  # def create
+  #   @user = User.find_by_email(session_params[:email])
+  #   if @user && @user.authenticate(session_params[:password])
+  #     login!(@user)
+  #     redirect_to users_path
+  #   else
+  #     redirect_to(root_url, notice: "Invalid email or password.")
+  #   end
+  # end
 
   def show
   end
