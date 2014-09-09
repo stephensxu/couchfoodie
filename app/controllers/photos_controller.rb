e919250d66b279e2561665192104c8857c9c333b
Stephens Xu <stephensxub@gmail.com>
2014-09-08 22:51:00 -0700

    added routes and controller action for creating new photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,19 @@
 class PhotosController < ApplicationController
+  def create
+    @photo = @kitchen.photos.new(photo_params)
+
+    if @photo.save
+      redirect_to user_path(current_user), :notice => 'Photo was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def photo_params
+    params.require(:photo).permit(:picture)
+  end
 end
