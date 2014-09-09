5b51613e26514412916a73081538cae36defe82f
Stephens Xu <stephensxub@gmail.com>
2014-09-09 01:08:53 -0700

    photo now has_one :kitchen_displaying_as_front_page

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -17,10 +17,10 @@ require 'file_size_validator'
 
 class Photo < ActiveRecord::Base
   belongs_to :kitchen
-  # has_one :kitchen, :inverse_of => :front_page_photo
+  has_one :kitchen_displaying_as_front_page, :class_name => "Kitchen", :inverse_of => :front_page_photo
 
   mount_uploader :picture, KitchenPhotosUploader
 
-  # validates :picture, :presence => true, 
-  #           :file_size => { :maximum => 5.megabytes.to_i }
+  validates :picture, :presence => true, 
+            :file_size => { :maximum => 5.megabytes.to_i }
 end
