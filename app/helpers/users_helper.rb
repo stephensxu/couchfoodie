4ce524520a01bb5624e7ec718db21bbbdc57bf24
Stephens Xu <stephensxub@gmail.com>
2014-09-11 21:30:41 -0700

    added user_photo_tag helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def user_photo_url(user, params = {})
+    uri.query = Addressable::URI.parse(user.image)
+    uri.query = params.to_param
+    uri.to_s
+  end
+
+  def user_photo_tag(user, params = {})
+    image_tag(user_photo_url(user, params))
+  end
 end
