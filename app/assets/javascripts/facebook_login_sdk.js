ae34ab6d447c3499f548a673493744bce4e9ed53
Stephens Xu <stephensxub@gmail.com>
2014-09-12 19:09:34 -0700

    using ENV to dynamically extract facebookAppId

--- app/assets/javascripts/facebook_login_sdk.js
+++ app/assets/javascripts/facebook_login_sdk.js
@@ -32,7 +32,7 @@
 
   window.fbAsyncInit = function() {
   FB.init({
-    appId      : 804613432924939,
+    appId      : CouchFoodie.config.facebookAppId,
     cookie     : true,  // enable cookies to allow the server to access 
                         // the session
     xfbml      : true,  // parse social plugins on this page
