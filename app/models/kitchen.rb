873fb725b14c65f2c7ffbe2c787a5a4e4040847f
Stephens Xu <stephensxub@gmail.com>
2014-09-13 22:53:38 -0700

    added more styling to view page

--- app/models/kitchen.rb
+++ app/models/kitchen.rb
@@ -38,7 +38,7 @@ class Kitchen < ActiveRecord::Base
   scope :published, lambda { active.with_photo }
 
   validates :name, :presence => true, :uniqueness => true, :length => { :minimum => 6 , :maximum => 50 }
-  validates :menu, :presence => true, :length => { :in => (5..50)  }
+  validates :menu, :presence => true, :length => { :in => (5..250)  }
   validates :description, :length => { :maximum => 250  }
   validates :street_address, :presence => true, :length => { :minimum => 6, :maximum => 50 }
   validates :city, :presence => true, :length => { :in => (3..50) }
