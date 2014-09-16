5eaee3a085aa77463a85ec791beb4bf706fadf67
Stephens Xu <stephensxub@gmail.com>
2014-09-16 00:33:03 -0700

    jquery now enable submit on file upload

--- app/assets/javascripts/photo_submit.js
+++ app/assets/javascripts/photo_submit.js
@@ -3,8 +3,15 @@
 
 $(document).ready(function() {
   console.log("photo submit is ready");
-  if(!$('#photo_picture').val());
+  if(!$('#photo_picture').val()) {
     console.log("file field is empty");
    $('#photo_upload_submit').prop( "disabled", true );
    $('#photo_upload_submit').css({ opacity: 0.5 });
+  }
+
+  $('input:file').on("change", function() {
+    console.log("file input is filled");
+    $('#photo_upload_submit').prop('disabled', false);
+    $('#photo_upload_submit').css({ opacity: 1 });
+  });
 });
