e876b4e07a152a38a120b079fb094c50db88afdd
Stephens Xu <stephensxub@gmail.com>
2014-09-16 01:47:51 -0700

    added styling for photo_upload modal

--- app/assets/javascripts/photo_submit.js
+++ app/assets/javascripts/photo_submit.js
@@ -4,14 +4,17 @@
 $(document).ready(function() {
   console.log("photo submit is ready");
   if(!$('#photo_picture').val()) {
-    console.log("file field is empty");
    $('#photo_upload_submit').prop( "disabled", true );
    $('#photo_upload_submit').css({ opacity: 0.5 });
   }
 
   $('input:file').on("change", function() {
-    console.log("file input is filled");
     $('#photo_upload_submit').prop('disabled', false);
     $('#photo_upload_submit').css({ opacity: 1 });
+  });
+
+  $('#photo_upload_submit').on('mouseover', function() {
+    console.log("submit button is hovered on");
+    $('#photo_modal').fadeIn(200);
   });
 });
