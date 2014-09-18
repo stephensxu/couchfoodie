8342ecae59c8da575b042c3c6df49d84ee785e2f
Stephens Xu <stephensxub@gmail.com>
2014-09-18 02:22:38 -0700

    background worker working properly

--- app/workers/photo_worker.rb
+++ app/workers/photo_worker.rb
@@ -5,8 +5,10 @@ class PhotoWorker
   def perform(photo_id)
     puts "worker starting to process photo..."
     photo = Photo.find(photo_id)
-    puts "calling recreate versions on photosuploader..."
-    photo.picture.recreate_versions!
+    puts "photo object is #{photo}..."
+    puts "calling process_in_background on photo.picture..."
+    photo.picture.recreate_versions!(:gallery_fill, :gallery_fill_thumbnail, :gallery_fill_cover)
+    # photo.picture.process_in_background
     puts "recreate_versions sucess..."
     photo.processed_at = Time.zone.now
     puts "worker finished processing photo..."
