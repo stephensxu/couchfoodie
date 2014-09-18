9533f5388aaed7c750a8c709ce49b1a8c0a8f47e
Stephens Xu <stephensxub@gmail.com>
2014-09-18 01:01:51 -0700

    testing by deleting recreate versions

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -12,7 +12,7 @@ class PhotosController < ApplicationController
 
     if @photo.save
       p "photo is saved to the database"
-      PhotoWorkder.perform_async(@photo.id)
+      PhotoWorker.perform_async(@photo.id)
       redirect_to kitchens_users_path, :notice => 'Photo was successfully created.'
     else
       render :new
--- app/workers/photo_worker.rb
+++ app/workers/photo_worker.rb
@@ -4,7 +4,6 @@ class PhotoWorker
   def perform(photo_id)
     puts "worker starting to process photo"
     photo = Photo.find(photo_id)
-    photo.recreate_versions!
     photo.processed_at = Time.zone.now
     puts "worker finished processing photo"
     photo.save!
