bf2a30476fe99c88b1479071c6e0e2e9db4b0d3f
Stephens Xu <stephensxub@gmail.com>
2014-09-18 01:30:27 -0700

    rewrite perform method in photoworker

--- app/workers/photo_worker.rb
+++ app/workers/photo_worker.rb
@@ -3,11 +3,14 @@ class PhotoWorker
   sidekiq_options :retry => false
 
   def perform(photo_id)
-    puts "worker starting to process photo"
+    puts "worker starting to process photo..."
     photo = Photo.find(photo_id)
+    puts "calling recreate versions on photosuploader..."
+    KitchenPhotosUploader.recreate_versions!
+    puts "recreate_versions sucess..."
     photo.processed_at = Time.zone.now
-    puts "worker finished processing photo"
+    puts "worker finished processing photo..."
     photo.save!
-    puts "photo is saved"
+    puts "photo is saved..."
   end
 end
