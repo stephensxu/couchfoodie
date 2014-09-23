f418d7faaf3894a13b9c3bddc03e75b69e4dd4a9
Stephens Xu <stephensxub@gmail.com>
2014-09-23 14:54:11 -0700

    added sidekiq.rb initializer

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,7 @@
+Sidekiq.configure_server do |config|
+  config.redis = { :url => ENV['REDISTOGO_URL'], :namespace => "couchfoodie"}
+end
+
+Sidekiq.configure_client do |config|
+    config.redis = { :url => ENV['REDISTOGO_URL'], :namespace => "couchfoodie"}
+end
