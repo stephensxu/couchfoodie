28be28d337fee7ffcc4628438e8d5700b4b1467d
Stephens Xu <stephensxub@gmail.com>
2014-09-23 21:07:50 -0700

    commented out all contents in sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
-Sidekiq.configure_server do |config|
-  config.redis = { :url => ENV['REDISTOGO_URL'], :namespace => "couchfoodie"}
-end
+# Sidekiq.configure_server do |config|
+#   config.redis = { :url => ENV['REDISTOGO_URL'], :namespace => "couchfoodie"}
+# end
 
-Sidekiq.configure_client do |config|
-    config.redis = { :url => ENV['REDISTOGO_URL'], :namespace => "couchfoodie"}
-end
+# Sidekiq.configure_client do |config|
+#     config.redis = { :url => ENV['REDISTOGO_URL'], :namespace => "couchfoodie"}
+# end
