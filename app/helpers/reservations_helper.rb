fa243e00cc4c97f499e49621ef8e5aea3e9e6da5
Stephens Xu <stephensxub@gmail.com>
2014-09-25 16:53:38 -0700

    reservations now sort DESC by date, time; made correction to show_reservation_time method

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,6 +1,7 @@
 module ReservationsHelper
   def show_reservation_time(reservation)
-    time = reservation.reserve_time.strftime("%A %l:%M %p")
-    "%s, %s" % [reservation.reserve_date, time]
+    time = reservation.reserve_time.strftime("%l:%M %p")
+    date = reservation.reserve_date.strftime("%Y-%m-%d %A")
+    "%s %s" % [date, time]
   end
 end
