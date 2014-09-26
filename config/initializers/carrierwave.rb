6117ad4cb387f6b115cbde00176830a41c78f9c7
Stephens Xu <stephensxub@gmail.com>
2014-09-26 14:48:13 -0700

    added test env config to carrierwave.rb initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,3 +10,10 @@ CarrierWave.configure do |config|
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
   }
 end
+
+if Rails.env.test? or Rails.env.cucumber?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
+end
