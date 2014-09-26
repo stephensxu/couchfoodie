b9f0304fb7b864793d8d6c4957eb254089a43123
Stephens Xu <stephensxub@gmail.com>
2014-09-25 21:08:12 -0700

    deleted password and nickname from factorygirl user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -33,8 +33,5 @@
 FactoryGirl.define do
   factory :user do |u|
     u.email { Faker::Internet.email }
-    u.password { Faker::Internet.password(min_length = 6) }
-    u.nickname { Faker::Name.title }
-    u.password_confirmation { |u| u.password }
   end
 end
