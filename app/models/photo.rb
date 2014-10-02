373398c4560837431c6d815c1e18ec6ca1ddda82
Stephens Xu <stephensxub@gmail.com>
2014-10-01 21:07:40 -0700

    rake db:migrate

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,7 @@
 # Table name: photos
 #
 #  id                 :integer          not null, primary key
-#  picture            :string(255)
+#  picture            :string(255)      not null
 #  created_at         :datetime
 #  updated_at         :datetime
 #  kitchen_id         :integer          not null
--- spec/factories/photos.rb
+++ spec/factories/photos.rb
@@ -1,28 +1,21 @@
 # == Schema Information
 #
-# Table name: kitchens
+# Table name: photos
 #
-#  id                  :integer          not null, primary key
-#  name                :string(255)      not null
-#  description         :text
-#  street_address      :string(255)      not null
-#  city                :string(255)      not null
-#  state               :string(255)      not null
-#  zipcode             :string(255)      not null
-#  latitude            :float
-#  longtitude          :float
-#  created_at          :datetime         not null
-#  updated_at          :datetime         not null
-#  user_id             :integer          not null
-#  data_status         :string(255)      default("active"), not null
-#  front_page_photo_id :integer
-#  menu                :string(255)      not null
+#  id                 :integer          not null, primary key
+#  picture            :string(255)      not null
+#  created_at         :datetime
+#  updated_at         :datetime
+#  kitchen_id         :integer          not null
+#  picture_tmp        :string(255)
+#  processed_at       :datetime
+#  picture_processing :boolean          default(FALSE), not null
 #
 # Indexes
 #
-#  index_kitchens_on_front_page_photo_id  (front_page_photo_id)
-#  index_kitchens_on_user_id              (user_id)
+#  index_photos_on_kitchen_id  (kitchen_id)
 #
+
 
 
 FactoryGirl.define do
