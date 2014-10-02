ed314d60e9315dd5a57d583484ad3f422182cad9
Stephens Xu <stephensxub@gmail.com>
2014-10-01 20:18:04 -0700

    deleted session_params method in sessionController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,9 +22,4 @@ class SessionsController < ApplicationController
     redirect_to(root_url)
   end
 
-  private
-
-  def session_params
-    params.require(:session).permit(:email, :password)
-  end
 end
