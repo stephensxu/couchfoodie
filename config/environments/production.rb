86bca4704e7ca82abd41ee4739fc44e805243972
Stephens Xu <stephensxub@gmail.com>
2014-10-03 20:51:07 -0700

    added mandrill config to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,18 @@ Rails.application.configure do
 
   $stdout.sync = true
 
+  ### Mandrill config
+  
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV['MANDRILL_USERNAME'],
+    :password  => ENV['MANDRILL_PASSWORD'], # SMTP password is any valid API key
+    :authentication => 'login', # Mandrill supports 'plain' or 'login'
+    :domain => 'couchfoodie.io', # your domain to identify your server when connecting
+  }
+
 end
