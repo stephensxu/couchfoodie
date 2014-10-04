cdfa475d24c3812c379fae06de1ea032f7ccbe10
Stephens Xu <stephensxub@gmail.com>
2014-10-03 21:12:14 -0700

    added plain text and html email to views/user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,11 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default :from => "notice@codeunion.io"
+
+  def welcome_email(user)
+
+    mail :subject => "Mandrill rides the Rails!",
+         :to      => user.email,
+         :from    => "notice@codeunion.io"
+  end
+
 end
