11aa857b4459e842432ba08c8e046ff1757c27f8
Stephens Xu <stephensxub@gmail.com>
2014-10-05 15:29:26 -0700

    corrected typo in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
 
-  Sidekiq::Web.user Rack::Auth::Basic do |username, password|
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
     username == ENV['SIDEKIQ_USERNAME'] && password == ENV['SIDEKIQ_PASSWORD']
   end
 
