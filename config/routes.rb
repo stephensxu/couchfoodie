b44d7475990ce00f585672eca0775ca78c4c7022
Stephens Xu <stephensxub@gmail.com>
2014-10-05 15:35:47 -0700

    added a line break in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ Rails.application.routes.draw do
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
     username == ENV['SIDEKIQ_USERNAME'] && password == ENV['SIDEKIQ_PASSWORD']
   end
-
+  
   mount Sidekiq::Web => '/sidekiq'
 
 
