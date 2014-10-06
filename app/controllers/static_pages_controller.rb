59f213f5d359770f60ca92d9fc21917a1997c927
Stephens Xu <stephensxub@gmail.com>
2014-10-06 01:55:54 -0700

    added image to about page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def show
     if page_exists?(params[:page_name])
-      render 'static_pages/%s' % params[:page_name]
+      render "static_pages/#{params[:page_name]}"
     else
       render :text => '<h1>Not Found</h1>', :status => :not_found
     end
